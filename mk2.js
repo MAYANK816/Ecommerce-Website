7100fa3c227dd91caa17f6630cba145e8c0899ac
MAYANK <63389169+MAYANK816@users.noreply.github.com>
2020-11-05 10:30:55 +0530

    Add files via upload

---
+++ mk2.js
@@ -0,0 +1,50 @@
+function sub()
+{
+	confirm("Submitted!");
+
+}
+function f1()
+{
+	document.getElementById("demo_1").innerHTML=document.getElementById("crt_1").innerHTML;
+	
+}
+function f2()
+{
+document.getElementById("demo_2").innerHTML=document.getElementById("crt_2").innerHTML;	
+	
+}
+function f3()
+{
+	document.getElementById("demo_3").innerHTML=document.getElementById("crt_3").innerHTML;
+}
+function f4()
+{
+document.getElementById("demo_4").innerHTML=document.getElementById("crt_4").innerHTML;	
+	
+}
+function f5()
+{
+	document.getElementById("demo_5").innerHTML=document.getElementById("crt_5").innerHTML;
+	
+}
+function f6()
+{
+	
+	document.getElementById("demo_6").innerHTML=document.getElementById("crt_6").innerHTML;
+}
+function f7()
+{
+	document.getElementById("demo_7").innerHTML=document.getElementById("crt_7").innerHTML;
+	
+}
+function f8()
+{
+	
+	document.getElementById("demo_8").innerHTML=document.getElementById("crt_8").innerHTML;
+}
+
+function checkout()
+{
+	window.open('Checkoutpage.html')
+	
+}
